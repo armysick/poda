26efafe1ebfb42829a32b77fea7a973bf4418800
Gustavo Pinto <gustavo.pinto@blip.pt>
2023-09-18 16:01:14 +0100

    gitignore db and update response based on mood

--- pages/views.py
+++ pages/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 
 def home_page_view(request):
-  return HttpResponse("Goodbye, World!")
+  if 'good' in 'feeling good':
+    return HttpResponse("Hello, World!")
+  else:
+    return HttpResponse("Goodbye, World!")
+
+
